1f288f918a21ab641ff99d865b381e6fd0bdaec0
ParkHongJin <ektkfk180@naver.com>
2022-04-18 21:54:16 +0900

    HelloControllerTest 추가

---
+++ src/test/java/com/example/myblog/controller/HelloControllerTest.java
@@ -0,0 +1,32 @@
+package com.example.myblog.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello_Test() throws Exception {
+        String hello = "hello World!";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
