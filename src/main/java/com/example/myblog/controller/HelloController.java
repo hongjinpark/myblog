6af5937e72649036827c0380c2e72957bf38b0f9
ParkHongJin <ektkfk180@naver.com>
2022-04-18 22:10:27 +0900

    HelloResponseDto 메소드 추가

--- src/main/java/com/example/myblog/controller/HelloController.java
+++ src/main/java/com/example/myblog/controller/HelloController.java
@@ -1,7 +1,9 @@
 package com.example.myblog.controller;
 
 
+import com.example.myblog.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +12,10 @@ public class HelloController {
    @GetMapping("/hello")
    public String helloController() {
         return "hello World!";
+   }
+
+   @GetMapping("/hello/dto")
+    public HelloResponseDto helloResponseDto(@RequestParam String name, @RequestParam String nickname) {
+       return new HelloResponseDto(name, nickname);
    }
 }
