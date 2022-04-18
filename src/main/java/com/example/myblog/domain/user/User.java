1aff5558080aa01e7327b32150fe5cfa6066727f
ParkHongJin <ektkfk180@naver.com>
2022-04-18 22:54:58 +0900

    User 엔티티 생성

---
+++ src/main/java/com/example/myblog/domain/user/User.java
@@ -0,0 +1,37 @@
+package com.example.myblog.domain.user;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String username; //아이디
+
+    @Column(nullable = false, length = 100)
+    private String password; //비밀번호
+
+    @Column(nullable = false, length = 50)
+    private String email; //이메일
+
+    @Column(nullable = false, length =50)
+    private String nickname; //닉네임
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
