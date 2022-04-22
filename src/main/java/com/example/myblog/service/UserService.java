27b706521937954a313c3c54ec9638eb5cb10a2c
ParkHongJin <ektkfk180@naver.com>
2022-04-22 21:29:25 +0900

    패스워드 암호화

--- src/main/java/com/example/myblog/service/UserService.java
+++ src/main/java/com/example/myblog/service/UserService.java
@@ -4,6 +4,7 @@ package com.example.myblog.service;
 import com.example.myblog.domain.user.User;
 import com.example.myblog.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,9 +13,11 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Transactional
     public Long save(User user) {
+        String hashPw = bCryptPasswordEncoder.encode(user.getPassword());
         return userRepository.save(user).getId();
     }
 }
