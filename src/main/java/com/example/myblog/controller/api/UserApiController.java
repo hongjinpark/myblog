9878ab7569f252c856a0c1c77e1a0ed41dd87deb
ParkHongJin <ektkfk180@naver.com>
2022-04-22 21:09:56 +0900

    UserApiController 생성
    save 메소드 추가

---
+++ src/main/java/com/example/myblog/controller/api/UserApiController.java
@@ -0,0 +1,21 @@
+package com.example.myblog.controller.api;
+
+
+import com.example.myblog.domain.user.UserSaveRequestDto;
+import com.example.myblog.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class UserApiController {
+
+    private final UserService userService;
+
+    @PostMapping("/api/v1/user")
+    public Long save(@RequestBody UserSaveRequestDto userSaveRequestDto) {
+        return userService.save(userSaveRequestDto.toEntity());
+    }
+}
