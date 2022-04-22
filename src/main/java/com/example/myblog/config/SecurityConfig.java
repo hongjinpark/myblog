b03f928924e10826070e240043d5a40535ee00eb
ParkHongJin <ektkfk180@naver.com>
2022-04-22 21:25:32 +0900

    BCryptPasswordEncoder : Bean으로 등록

--- src/main/java/com/example/myblog/config/SecurityConfig.java
+++ src/main/java/com/example/myblog/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.example.myblog.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -22,5 +24,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/auth/user/login")
                 //.loginProcessingUrl("/auth/api/v1/user/login")
                 .defaultSuccessUrl("/");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
