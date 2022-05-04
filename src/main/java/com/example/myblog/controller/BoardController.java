a7ff16efcc336b8b07f54952d747c7b0fdd83575
ParkHongJin <ektkfk180@naver.com>
2022-05-04 19:32:37 +0900

    BoardController 생성
    save 메소드 추가

---
+++ src/main/java/com/example/myblog/controller/BoardController.java
@@ -0,0 +1,14 @@
+package com.example.myblog.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+
+    @GetMapping("/board/save")
+    public String save() {
+        return "layout/board/board-save";
+    }
+}
