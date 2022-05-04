93d7d743f4a0e311c60565670a02d991d536651f
ParkHongJin <ektkfk180@naver.com>
2022-05-04 19:04:16 +0900

    update 메소드 추가
    회원수정 API 추가

--- src/main/java/com/example/myblog/controller/api/UserApiController.java
+++ src/main/java/com/example/myblog/controller/api/UserApiController.java
@@ -1,10 +1,12 @@
 package com.example.myblog.controller.api;
 
 
+import com.example.myblog.domain.user.User;
 import com.example.myblog.domain.user.UserSaveRequestDto;
 import com.example.myblog.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +19,10 @@ public class UserApiController {
     @PostMapping("/auth/api/v1/user")
     public Long save(@RequestBody UserSaveRequestDto userSaveRequestDto) {
         return userService.save(userSaveRequestDto.toEntity());
+    }
+
+    @PutMapping("/api/v1/user")
+    public Long update(@RequestBody User user) {
+        return userService.update(user);
     }
 }
