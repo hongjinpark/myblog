3af8497613a5d95421c016469e631e1e6be633bb
ParkHongJin <ektkfk180@naver.com>
2022-05-04 19:06:23 +0900

    AuthenticationManager 클래스를  Bean으로 등록

--- src/main/java/com/example/myblog/config/SecurityConfig.java
+++ src/main/java/com/example/myblog/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.example.myblog.config.auth.PrincipalDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,6 +23,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Override
