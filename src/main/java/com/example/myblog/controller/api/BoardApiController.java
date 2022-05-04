72d39b870599b06e3c1536b6b25518bc978b6633
ParkHongJin <ektkfk180@naver.com>
2022-05-04 19:38:21 +0900

    BoardApiController 생성
    어떤 사용자가 게시글을 작성하는지 알기 위해 @AuthenticationPrincipal 정보를 파라미터로 받음

---
+++ src/main/java/com/example/myblog/controller/api/BoardApiController.java
@@ -0,0 +1,24 @@
+package com.example.myblog.controller.api;
+
+
+import com.example.myblog.config.auth.PrincipalDetail;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class BoardApiController {
+
+    private final BoardService boardService;
+
+    /**
+     * 글작성 API
+     */
+    @PostMapping("/api/v1/board")
+    public Long save(@RequestBody BoardSaveRequestDto boardSaveRequestDto, @AuthenticationPrincipal PrincipalDetail principalDetail) {
+        return boardSaveRequestDto.save(boardSaveRequestDto, principalDetail.getUser());
+    }
+}
