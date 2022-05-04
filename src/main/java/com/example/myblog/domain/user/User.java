6e76c39278f1bd65d030dc1b53839cf592bed283
ParkHongJin <ektkfk180@naver.com>
2022-05-04 19:05:42 +0900

    update 메소드 추가

--- src/main/java/com/example/myblog/domain/user/User.java
+++ src/main/java/com/example/myblog/domain/user/User.java
@@ -35,11 +35,26 @@ public class User {
     @Column(nullable = false)
     private Role role;
 
+
+    /**
+     * 비밀번호 암호화 메소드
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * 권한 메소드
+     */
     public String getRoleKey() {
         return this.role.getKey();
+    }
+
+    /**
+     * 회원수정 메소드
+     */
+    public void update(String password, String nickname) {
+        this.password = password;
+        this.nickname = nickname;
     }
 }
