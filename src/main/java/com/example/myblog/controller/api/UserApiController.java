bf6cc75305050954c73d669d5b5e5ac364e0dfb2
ParkHongJin <ektkfk180@naver.com>
2022-05-04 20:14:42 +0900

    update 메소드 수정
    @AuthenticationPrincipal에 PrincipalDetail타입으로 파라미터를 받아서 유저 정보를 얻어옴

--- src/main/java/com/example/myblog/controller/api/UserApiController.java
+++ src/main/java/com/example/myblog/controller/api/UserApiController.java
@@ -1,10 +1,12 @@
 package com.example.myblog.controller.api;
 
 
+import com.example.myblog.config.auth.PrincipalDetail;
 import com.example.myblog.domain.user.User;
 import com.example.myblog.domain.user.UserSaveRequestDto;
 import com.example.myblog.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,7 +24,8 @@ public class UserApiController {
     }
 
     @PutMapping("/api/v1/user")
-    public Long update(@RequestBody User user) {
-        return userService.update(user);
+    public Long update(@RequestBody User user, @AuthenticationPrincipal PrincipalDetail principalDetail) {
+        userService.update(user, principalDetail);
+        return user.getId();
     }
 }
