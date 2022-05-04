d17be4c094fa00f1410ed2fad2387b14a2799760
ParkHongJin <ektkfk180@naver.com>
2022-05-04 19:54:56 +0900

    BoardService 생성
    save 메소드 추가

---
+++ src/main/java/com/example/myblog/service/BoardService.java
@@ -0,0 +1,25 @@
+package com.example.myblog.service;
+
+
+import com.example.myblog.domain.board.Board;
+import com.example.myblog.domain.board.BoardRepository;
+import com.example.myblog.domain.user.User;
+import com.example.myblog.dto.board.BoardSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    @Transactional
+    public Long save(BoardSaveRequestDto boardSaveRequestDto, User user) {
+        boardSaveRequestDto.setUser(user);
+        return boardRepository.save(boardSaveRequestDto.toEntity()).getId();
+    }
+}
