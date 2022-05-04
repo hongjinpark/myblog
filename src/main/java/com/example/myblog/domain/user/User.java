e105ce02705f107be36be6ee1c7cadae614c4dfe
ParkHongJin <ektkfk180@naver.com>
2022-05-04 17:32:25 +0900

    getRoleKey 메소드 추가

--- src/main/java/com/example/myblog/domain/user/User.java
+++ src/main/java/com/example/myblog/domain/user/User.java
@@ -38,4 +38,8 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
 }
