c52beea8fe149a76a6474e6fb1fcb9d5023a6002
ParkHongJin <ektkfk180@naver.com>
2022-05-04 19:20:01 +0900

    PrincipalDetail 파라미터 추가
    update된 유저 정보를 principalDetail에 집어넣음

--- src/main/java/com/example/myblog/service/UserService.java
+++ src/main/java/com/example/myblog/service/UserService.java
@@ -1,9 +1,11 @@
 package com.example.myblog.service;
 
 
+import com.example.myblog.config.auth.PrincipalDetail;
 import com.example.myblog.domain.user.User;
 import com.example.myblog.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,9 +25,10 @@ public class UserService {
     }
 
     @Transactional
-    public Long update(User user) {
+    public Long update(User user, @AuthenticationPrincipal PrincipalDetail principalDetail) {
         User userEntity = userRepository.findById(user.getId()).orElseThrow(() -> new IllegalArgumentException("해당 회원이 없습니다. id=" + user.getId()));
         userEntity.update(bCryptPasswordEncoder.encode(user.getPassword()), user.getNickname());
+        principalDetail.setUser(userEntity); //추가
         return userEntity.getId();
     }
 }
