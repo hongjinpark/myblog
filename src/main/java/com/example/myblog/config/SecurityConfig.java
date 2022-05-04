c331e9b3660c40b0f7dae066c954aaf378ca19fd
ParkHongJin <ektkfk180@naver.com>
2022-05-04 15:49:53 +0900

    configure(AuthenticationManagerBuilder auth) 이 메소드를 오버라이딩

--- src/main/java/com/example/myblog/config/SecurityConfig.java
+++ src/main/java/com/example/myblog/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.myblog.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,6 +12,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(principalDetailService).passwordEncoder(bCryptPasswordEncoder());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
