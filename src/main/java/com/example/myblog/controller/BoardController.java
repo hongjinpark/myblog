e84715d3e1843b5606e2d1fe0fc4b8dbf592d980
ParkHongJin <ektkfk180@naver.com>
2022-05-07 19:28:54 +0900

    detail 메소드 추가

--- src/main/java/com/example/myblog/controller/BoardController.java
+++ src/main/java/com/example/myblog/controller/BoardController.java
@@ -1,14 +1,27 @@
 package com.example.myblog.controller;
 
 
+import com.example.myblog.service.BoardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class BoardController {
+
+    private final BoardService boardService;
 
     @GetMapping("/board/save")
     public String save() {
         return "layout/board/board-save";
+    }
+
+    @GetMapping("/board/{id}")
+    public String detail(@PathVariable Long id, Model model) {
+        model.addAttribute("board", boardService.detail(id));
+        return "layout/board/board-detail";
     }
 }
