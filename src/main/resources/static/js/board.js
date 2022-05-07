446659f5b0ca4fd1c41c1979840d5b43141ec16f
ParkHongJin <ektkfk180@naver.com>
2022-05-07 21:45:50 +0900

    update 로직 추가

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -9,6 +9,10 @@ let index = {
         $("#btn-delete").on("click", () => {
             this.deleteById();
         });
+
+        $("#btn-update").on("click", () => {
+            this.update();
+        });
     },
 
     save: function () {
@@ -44,6 +48,28 @@ let index = {
         }).fail(function (err) {
             alert(JSON.stringify(err));
         });
-    }
+    },
+
+    update: function () {
+        let id = $("#id").val();
+
+        let data = {
+            title: $("#title").val(),
+            content: $("#content").val()
+        }
+
+        $.ajax({
+            type: "PUT",
+            url: "/api/v1/board/" + id,
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+        }).done(function (res) {
+            alert("글수정이 완료되었습니다.");
+            location.href = "/";
+        }).fail(function (err) {
+            alert(JSON.stringify(err));
+        });
+    },
 }
 index.init();
