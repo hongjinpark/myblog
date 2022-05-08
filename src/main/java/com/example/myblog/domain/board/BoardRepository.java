78d3765b00a20709d0bf7d6d5318a63f9b795b20
ParkHongJin <ektkfk180@naver.com>
2022-05-08 14:45:46 +0900

    update 쿼리 추가
    수정 반영을 위한 @Modifying 사용

--- src/main/java/com/example/myblog/domain/board/BoardRepository.java
+++ src/main/java/com/example/myblog/domain/board/BoardRepository.java
@@ -1,6 +1,13 @@
 package com.example.myblog.domain.board;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    @Modifying
+    @Query("update Board b set b.count = b.count + 1 where b.id = :id")
+    int updateCount(Long id);
 }
