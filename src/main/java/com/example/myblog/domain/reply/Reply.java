9c88a98b10ed19489574710d2121c85dfe47a5fe
ParkHongJin <ektkfk180@naver.com>
2022-05-08 17:43:38 +0900

    Reply 엔티티 생성

---
+++ src/main/java/com/example/myblog/domain/reply/Reply.java
@@ -0,0 +1,35 @@
+package com.example.myblog.domain.reply;
+
+
+import com.example.myblog.domain.BaseTimeEntity;
+import com.example.myblog.domain.board.Board;
+import com.example.myblog.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+public class Reply extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 500)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "boardId")
+    private Board board;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+}
