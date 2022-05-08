ff13c517bcb898a82865e006fe751254fe21e68a
ParkHongJin <ektkfk180@naver.com>
2022-05-08 15:31:53 +0900

    findByTitleContainingOrContentContaining 메소드 추가

--- src/main/java/com/example/myblog/domain/board/BoardRepository.java
+++ src/main/java/com/example/myblog/domain/board/BoardRepository.java
@@ -1,5 +1,7 @@
 package com.example.myblog.domain.board;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -10,4 +12,6 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     @Modifying
     @Query("update Board b set b.count = b.count + 1 where b.id = :id")
     int updateCount(Long id);
+
+    Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
 }
