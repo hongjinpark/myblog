800d400ecf71a90137e4c2637d82483bf2fa47b6
ParkHongJin <ektkfk180@naver.com>
2022-05-09 21:04:55 +0900

    BoardRepository 수정
    @Transactional, @Param 어노테이션 추가

--- src/main/java/com/example/myblog/domain/board/BoardRepository.java
+++ src/main/java/com/example/myblog/domain/board/BoardRepository.java
@@ -5,13 +5,17 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
+    @Transactional
     @Modifying
     @Query("update Board b set b.count = b.count + 1 where b.id = :id")
     int updateCount(Long id);
 
-    Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
+    Page<Board> findByTitleContainingOrContentContaining(
+            @Param("title") String title, @Param("content") String content, Pageable pageable);
 }
